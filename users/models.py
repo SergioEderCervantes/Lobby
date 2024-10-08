cf55921baf8d9a3a5bac55020d6a1c4de90eed61
Luis Flores <al348961@edu.uaa.mx>
2024-10-07 21:58:36 -0600

    Cambios de user

--- users/models.py
+++ users/models.py
@@ -1,9 +1,44 @@
 from django.db import models
-from django.allauth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
-class CustomUser(User):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+class User(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('AD', 'Admin'),
+        ('US', 'User'),
+    ]
+
+    username = models.CharField(
+        _("Nombre de usuario"), blank=False, max_length=30, unique=True, error_messages={
+            "unique": _("Ya existe un usuario con este nombre de usuario, por favor verifíquelo."),
+        },
+    )
+
+    nombre = models.CharField(
+        ("Nombre de usuario"), blank=False, max_length=30, default=""
+    )
+    
+    apellido = models.CharField(
+        _("Apellido de usuario"),
+        blank=False,
+        max_length=30,
+        default="",
+    )
+
+    tipo_usuario = models.CharField(
+        _("Tipo de usuario"),
+        blank=False,
+        max_length=2,
+        choices=USER_TYPE_CHOICES,
+        default="AU",
+    )
+
+    email = models.EmailField(
+        _("Correo electrónico"),
+        blank=False,
+        unique=True,
+        error_messages={
+            "unique": _("Ya existe un usuario con este correo electrónico, por favor verifíquelo."),
+        },
+    )
+
