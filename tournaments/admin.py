26da59693f585ec77d7590f19fb5980549d0c385
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2024-10-18 22:07:58 -0600

    que dios me ayude a lo que voy a hacer NO PUSHEAR HASTA QUE TODO SALGA BIEN

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
-
+from custom_admin.admin import admin_site
+from .models import users_prueba, tournament_prueba, users_admin
 # Register your models here.
+
+admin_site.register(users_prueba, users_admin)
+admin_site.register(tournament_prueba)
--- tournaments/models.py
+++ tournaments/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+
+
+class tournament_prueba(models.Model):
+    MODOS_DE_TORNEOS = models.TextChoices("Modo de Torneo", "Single 2v2")
+    TEXTO_DESCRIPCION = "El tipo de torneo que sera, ya sea de equipos de n jugadores o de un jugador en solitario"
+    tournament_id = models.BigAutoField(primary_key=True)
+    tournament_name = models.CharField(max_length=30)
+    tournament_mode = models.CharField(max_length=20, choices=MODOS_DE_TORNEOS, default="Un Jugador", help_text=TEXTO_DESCRIPCION)
+    date = models.DateTimeField()
+    def __str__(self):
+        return self.tournament_name
+    
+    class Meta:
+        db_table = 'tournament_pruebas'
+        verbose_name = 'torneo'
+
+
+
+
+class users_prueba(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    tournament_id = models.ForeignKey(tournament_prueba, verbose_name="ID del torneo que esta inscrito",on_delete=models.CASCADE)
+    def __str__(self):
+        return self.first_name + self.last_name
+    class Meta:
+        db_table = 'users_prueba'
+        verbose_name = 'prueba usuario'
+
+
+class users_admin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
