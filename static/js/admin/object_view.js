0600d7b07054b536792d58c4002fce120fb4f575
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2024-10-21 11:50:34 -0600

    Agregado link que te mande a la pagina de cambio de estado

--- static/js/admin/object_view.js
+++ static/js/admin/object_view.js
@@ -13,3 +13,14 @@ document.getElementById('create_tournament').addEventListener('click', function 
         })
     })
 })
+
+// Cambiar la url para que la redirija al change
+const change_link = document.getElementById('change_info')
+if (change_link){
+    const current_url = change_link.href;
+    console.log(current_url);
+    const new_url = current_url.replace("/view/#", "/change");
+    console.log(new_url);
+    change_link.href = new_url;
+}
+
