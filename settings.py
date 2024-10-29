753975ed590df508f112638d16afaf0f0a4b1a46
dany Carabez <al267187@edu.uaa.mx>
2024-10-29 12:36:11 -0600

    regulacion

--- settings.py
+++ settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'lobby',
         'USER': 'root',
-        'PASSWORD': 'PablitoClavoUnClavitoEnSuCalvaDeCalvito',
+        'PASSWORD': 'DJosking_al267187',
         'HOST': 'localhost',
         'PORT': '3306'
     }
