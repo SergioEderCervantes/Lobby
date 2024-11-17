70cb7d5f083814bd1829064ba2b3257dfb8cc9a1
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2024-11-17 13:10:07 -0600

    Agregado modelo de torneo funcional junto con el user y su relacion m-n de usuariosTorneo

--- custom_admin/admin.py
+++ custom_admin/admin.py
@@ -1,12 +1,13 @@
-from django.contrib.admin import AdminSite
+from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path
 from users.models import User
 from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
 # Register your models here.
 
 
-class custom_admin_site(AdminSite):
+class custom_admin_site(admin.AdminSite):
     site_header = 'Lobby Bar Administration'
     
     def get_urls(self):
@@ -22,5 +23,14 @@ class custom_admin_site(AdminSite):
 
 admin_site = custom_admin_site(name='customAdmin')
 
+# Clases manejadoras del display de los modelos del allAuth
+class EmailAddressAdmin(admin.ModelAdmin):
+    list_display = ('email', 'user', 'verified', 'primary')
+
+class SocialAccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'provider', 'uid')
+    
+    
 admin_site.register(User)
-admin_site.register(EmailAddress)
+admin_site.register(EmailAddress, EmailAddressAdmin)
+admin_site.register(SocialAccount,SocialAccountAdmin)
--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Torneo
+from custom_admin.admin import admin_site
 # Register your models here.
+
+class torneoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_torneo','nombre_juego','modo_torneo','fecha', 'is_defined')
+    readonly_fields = ('is_defined',)
+
+
+admin_site.register(Torneo,torneoAdmin)
