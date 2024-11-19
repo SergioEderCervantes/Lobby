897fa0ee4c564d60f79f6efaee445cf3d21b5a65
estebanesparza <estebanlive09@gmail.com>
2024-11-19 10:52:30 -0600

    Primer avance reservaciones

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Reservation(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    num_personas = models.PositiveIntegerField()
+    comentarios = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Reserva de {self.nombre} para {self.fecha} a las {self.hora}"
