fec108e12d3db4a5364470a631184b22b305125a
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2024-12-02 14:42:38 -0600

    Acomodado todo el torneo y las vistas, faltan ajustes del front pero de back ya esta

--- tournaments/views.py
+++ tournaments/views.py
@@ -9,29 +9,36 @@ from settings import STATICFILES_DIRS
 # Create your views here.
 
 def tournaments(request):
-    tournaments = Torneo.objects.all().annotate(num_users = Count('usuarios_torneo'))
-
-    #tournaments = [Tournament(t.nombre_torneo,t.nombre_juego,"",t.fecha,"","",t.imagen,t.usuarios_torneo.all().count()) for t in data]
+    tournaments = Torneo.objects.all().annotate(num_players = Count('jugadores_inscritos'))
 
     return render(request, 'tournaments.html', {'tournaments': tournaments})
 
 
 def tournament_detail(request, name):
-    torneo = get_object_or_404(Torneo,nombre_torneo=name)
-    matchups_ready = torneo.is_defined
+        
+    # Obtener el torneo y su número de jugadores inscritos
+    torneo = get_object_or_404(Torneo, nombre_torneo=name)
+
+    # Obtener los jugadores inscritos
+    players = torneo.usuarios_inscritos()
+    num_players = len(players)
+
     svg_data = None
-    if(matchups_ready):
+    if(torneo.is_defined):
+        # Carga el SVG
         parent_dir = STATICFILES_DIRS[0]
         file_name= str(torneo.pk) + '.svg'
         file_path = os.path.join(parent_dir,'svg_tournaments', file_name)
         with open(file_path, 'r') as svg_file:
             svg_data = svg_file.read()
             
-    # Sacar solo la data del torneo necesaria para el render del html
-    tournament_data = Tournament(torneo.nombre_torneo,torneo.imagen,torneo.nombre_juego,torneo.get_modo_torneo_display(),
-                                 torneo.fecha, torneo.descripcion, torneo.reglas.splitlines(),
-                                 torneo.usuarios_torneo.all().count())
-    
+    context = {
+        'torneo': torneo,
+        'svg_data': svg_data,
+        'reglas': torneo.reglas_como_lista(),
+        'num_players': num_players,
+        'players': players
+    }
 
 
-    return render(request, 'tournament_detail.html', {'tournament' : tournament_data,'svg_data': svg_data})
+    return render(request, 'tournament_detail.html',context)
