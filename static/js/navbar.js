1ad2cd55876b361a0e542c0e4268cf6a1da51315
dany Carabez <al267187@edu.uaa.mx>
2024-12-05 20:51:13 -0600

    comments and fixs

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,27 +1,4 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const navbar = document.querySelector(".navbar");
-//     const landpage = document.querySelector(".landpage");
 
-//     // Si la landing page no existe (páginas como Restaurante, etc.)
-//     if (!landpage) {
-//         navbar.classList.add("sticky"); // Fija la navbar automáticamente
-//         return;
-//     }
-
-//     // Si hay una landing page, manejar la transición entre estados
-//     document.addEventListener("scroll", () => {
-//         const landBottom = landpage.getBoundingClientRect().bottom;
-
-//         if (landBottom <= 125 ) {
-//             // Cambia el estilo de la navbar cuando la landing page desaparezca
-//             navbar.classList.add("sticky");
-//         } 
-//         else {
-//             // Restaura el estilo de la navbar mientras la landing page esté visible
-//             navbar.classList.remove("sticky");
-//         }
-//     });
-// });
  const icons = document.querySelectorAll('.icon');
 
  icons.forEach(icon => {
