79c1dfcfc9ae449c9168bbb5f4300857fe7b41db
estebanesparza <estebanlive09@gmail.com>
2024-12-07 18:39:01 -0600

    Avance consolas

--- static/js/validarFecha.js
+++ static/js/validarFecha.js
@@ -24,8 +24,10 @@ async function checarDisp() {
         if (!response.ok) {
             const errorData = await response.json();
             console.error("Error:", errorData.error);
-            alert(`Error: ${errorData.error}`); // Usar backticks para interpolación
+            //alert(`Error: ${errorData.error}`); // Usar backticks para interpolación
+            mostrarError(errorData.error);
             return;
+           
         }
 
         const data = await response.json();
@@ -33,7 +35,7 @@ async function checarDisp() {
         //alert(`Disponibilidad: ${JSON.stringify(data.disponibilidad)}`); // Mostrar disponibilidad
     } catch (error) {
         //console.error("Error inesperado:", error);
-        //alert(`Error inesperado: ${error.message}`);
+        alert(`Error inesperado: ${error.message}`);
     }
 }
 
@@ -41,6 +43,8 @@ function mostrarError(mensaje) {
     // Muestra una ventana modal con el error
     const modal = document.getElementById("error-modal");
     const modalMessage = document.getElementById("modal-mensaje");
+    const form = document.getElementById("reservation-form");
+    const fields = form.querySelectorAll(".campoForm");
 
     modalMessage.textContent = mensaje;
     modal.style.display = "block";
@@ -49,8 +53,24 @@ function mostrarError(mensaje) {
     const closeButton = document.getElementById("close-modal");
     closeButton.addEventListener("click", () => {
         modal.style.display = "none";
+        
+        // Resetea el valor del input fecha
+        const inputFecha = document.getElementById("fecha");
+        inputFecha.value = "";  // Resetea el valor del campo de fecha
+        
+        fields.forEach((field, index) => {
+            if (index > 0) {
+                field.classList.remove("visible");  // Elimina la clase 'visible' para hacer el campo invisible
+            }
+            const input = field.querySelector("input, textarea");
+            if (input) {
+                input.value = "";  // Resetea el valor del campo
+            }
+        });
+
     });
 }
+
 
 function formatearCampoError(campo) {
     // Agregar un estilo de error al campo
