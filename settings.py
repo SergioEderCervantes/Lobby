30cd271ca21998dddb24e06450970fa89f7973e6
dany Carabez <al267187@edu.uaa.mx>
2024-12-11 02:13:00 -0600

    responsive home y asociados

--- settings.py
+++ settings.py
@@ -160,11 +160,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "node_modules",   
+    BASE_DIR / "node_modules",
+    BASE_DIR / "static",   
 ]
 
 
